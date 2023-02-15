dd9c4496e5525ecf88166da0396b7edb9147a1df
Vinko Mlacic <vinkomlacic@outlook.com>
2023-02-16 00:20:53 +0100

    Fix bug when token cannot be decoded after secret key changes

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
 from sqlalchemy.orm import Session
 
 from app import schemas
@@ -25,9 +26,13 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     """
     Retrieves the current user from the provided access token and verifies it exists in the database.
 
-    If the user does not exist in the database, this function silently fails and returns None.
+    If the user does not exist in the database or there was a problem decoding the token, this function silently
+    fails and returns None.
     """
-    user_base = decode_token(token)
+    try:
+        user_base = decode_token(token)
+    except JWTError:
+        return None
 
     user_repository = UserRepository(db)
     user = user_repository.get_user_by_username(user_base.username)
